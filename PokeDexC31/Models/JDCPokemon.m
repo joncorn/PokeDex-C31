9ba9b6cb39752fdb2f02d0d4f073731fd04673be
jon <jdcorn@live.com>
2020-01-28 10:49:45 -0700

    implement pokemon.m file

--- PokeDexC31/Models/JDCPokemon.m
+++ PokeDexC31/Models/JDCPokemon.m
@@ -17,9 +17,9 @@
     self = [self init];
     if (self)
     {
-        _name = name;
+        _name = [name copy];
         _identifier = identifier;
-        _abilities = abilities;
+        _abilities = [abilities copy];
     }
     return self;
 }
@@ -30,7 +30,28 @@
 
 - (instancetype) initWithDictionary:(NSDictionary<NSString *,id> *)dictionary
 {
+    NSString *name = dictionary[@"name"];
+    NSInteger identifier = [dictionary[@"id"] integerValue];
+    NSArray *abilitiesArray = dictionary[@"abilities"];
     
+    if (![name isKindOfClass: [NSString class]] || ![abilitiesArray isKindOfClass:[NSArray class]])
+    {
+        return nil;
+    }
+    // our source of truth
+    NSMutableArray *abilities = [NSMutableArray new];
+    // for loop to pull ou abilities name
+    for (NSDictionary *dictionary in abilitiesArray)
+    {
+        NSDictionary *abilityDictrionary = dictionary[@"ability"];
+        NSString *abilityName = abilityDictrionary[@"name"];
+        if (!abilityName)
+        {
+            continue;
+        }
+        [abilities addObject:abilityName];
+    }
+    return [self initWithName:name identifier:identifier abilities:abilities];
 }
 
 @end
